6e170472d1e08e256a200a57d2b2c1392bb32a9d
quiet_di <razinkov_dmitrij@mail.ru>
2015-03-08 00:22:19 +0300

    list fragment

---
+++ app/src/main/java/com/quiet/test/ContactFragment.java
@@ -0,0 +1,98 @@
+package com.quiet.test;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+/**
+ * Created by Дмитрий on 06.03.2015.
+ */
+public class ContactFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private CursorAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // create adapter once
+        Context context = getActivity();
+        int layout = R.layout.contact_list_item;
+        Cursor c = null; // there is no cursor yet
+        int flags = 0; // no auto-requery! Loader requeries.
+        mAdapter = new SimpleCursorAdapter(context, layout, c, FROM, TO, flags);
+
+    }
+
+
+
+    // columns requested from the database
+    private static final String[] PROJECTION = {
+            ContactsContract.Contacts._ID, // _ID is always required
+            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY // that's what we want to display
+    };
+
+    // and name should be displayed in the text1 textview in item layout
+    private static final String[] FROM = { ContactsContract.Contacts.DISPLAY_NAME_PRIMARY };
+    private static final int[] TO = { R.id.text1 };
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        // load from the "Contacts table"
+        Uri contentUri = ContactsContract.Contacts.CONTENT_URI;
+
+        // no sub-selection, no sort order, simply every row
+        // projection says we want just the _id and the name column
+        return new CursorLoader(getActivity(),
+                contentUri,
+                PROJECTION,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Once cursor is loaded, give it to adapter
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // on reset take any old cursor away
+        mAdapter.swapCursor(null);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+         super.onListItemClick(l, v, position, id);
+         Toast.makeText(getActivity(), position + "1", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // each time we are started use our listadapter
+        setListAdapter(mAdapter);
+        // and tell loader manager to start loading
+        getLoaderManager().initLoader(0, null, this);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
