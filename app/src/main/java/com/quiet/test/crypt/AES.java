e7a951b27881c542eb1a7ae0529e7ac9b16c8ddd
quiet_di <razinkov_dmitrij@mail.ru>
2015-04-12 14:25:01 +0300

    RSA AES

---
+++ app/src/main/java/com/quiet/test/crypt/AES.java
@@ -0,0 +1,57 @@
+package com.quiet.test.crypt;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+
+public class AES
+{
+    private SecretKey encryptionKey;
+
+    public void generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        this.encryptionKey= keyGen.generateKey();
+    }
+
+
+    public String encrypt(String plainText) throws Exception
+    {
+        Cipher cipher = getCipher(Cipher.ENCRYPT_MODE);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+
+        return Base64.encodeBase64String(encryptedBytes);
+    }
+
+    public String decrypt(String encrypted) throws Exception
+    {
+        Cipher cipher = getCipher(Cipher.DECRYPT_MODE);
+        byte[] plainBytes = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+        return new String(plainBytes);
+    }
+
+    private Cipher getCipher(int cipherMode)
+            throws Exception
+    {
+        String encryptionAlgorithm = "AES";
+        SecretKeySpec keySpecification = new SecretKeySpec(
+                encryptionKey.getEncoded(), encryptionAlgorithm);
+        Cipher cipher = Cipher.getInstance(encryptionAlgorithm);
+        cipher.init(cipherMode, keySpecification);
+
+        return cipher;
+    }
+
+    public SecretKey getEncryptionKey() {
+        return encryptionKey;
+    }
+
+    public void setEncryptionKey(SecretKey encryptionKey) {
+        this.encryptionKey = encryptionKey;
+    }
+}
---
+++ app/src/main/java/com/quiet/test/crypt/RSA.java
@@ -0,0 +1,93 @@
+package com.quiet.test.crypt;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+
+public class RSA {
+    private PrivateKey privateKey;
+
+    private PublicKey publicKey;
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(PrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(PublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public void generateKey() throws NoSuchAlgorithmException {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(512);
+        KeyPair pair = generator.genKeyPair();
+        privateKey=pair.getPrivate();
+        publicKey=pair.getPublic();
+    }
+
+    public BigInteger getPublicExponent() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        RSAPublicKeySpec pub = fact.getKeySpec(publicKey,
+                RSAPublicKeySpec.class);
+        return pub.getPublicExponent();
+    }
+    public BigInteger getPrivateExponent() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        RSAPrivateKeySpec pr = fact.getKeySpec(privateKey,
+                RSAPrivateKeySpec.class);
+        return pr.getPrivateExponent();
+    }
+    public BigInteger getModulus() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        RSAPrivateKeySpec pr = fact.getKeySpec(privateKey,
+                RSAPrivateKeySpec.class);
+        return pr.getModulus();
+    }
+
+    public byte[] strToByteArray(String s) {
+        return s.getBytes(Charset.forName("UTF-8"));
+    }
+    public String byteArrayToString(byte[] bytes) {
+        return new String(bytes);
+    }
+
+    public byte[] rsaEncrypt(byte[] data) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] cipherData = cipher.doFinal(data);
+        return cipherData;
+    }
+
+    public byte[] rsaDecrypt(byte[] data) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] cipherData=cipher.doFinal(data);
+        return cipherData;
+    }
+
+    public byte[] concatArray(byte[] a, byte[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+        byte[] c= new byte[aLen+bLen];
+        System.arraycopy(a, 0, c, 0, aLen);
+        System.arraycopy(b, 0, c, aLen, bLen);
+        return c;
+    }
+}
